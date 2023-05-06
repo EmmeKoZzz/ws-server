c5d9c589a6e11869ff0de02d6ba133ec5cbe3579
Emme <emme.ko.zzz@gmail.com>
2023-05-06 09:00:41 -0400

    add docker compose and set config to use Redis

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,9 +4,20 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         roomName = self.scope["path_remaining"]
-        print(self.scope)
         self.accept();
         self.send(text_data=json.dumps({
             'type':'connected',
             'payload': 'Successful Conection.',
         }))
+
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        payload = data['payload']
+        self.send(text_data=json.dumps({
+            'type': "chatMessage",
+            'payload': f'{payload} recived!'
+        })) 
+
+    def disconnect(self, code):
+        pass
+        
--- server/settings.py
+++ server/settings.py
@@ -74,6 +74,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'server.wsgi.application'
 ASGI_APPLICATION = 'server.asgi.application'
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
